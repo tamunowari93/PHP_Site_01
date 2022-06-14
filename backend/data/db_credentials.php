ef63a7f69e363bc84a5c3d3805335d220bde4946
Tamunowari93 <tj02342@georgiasouthern.edu>
2022-06-14 11:13:07 -0400

    Database Connection 01

--- backend/data/db_credentials.php
+++ backend/data/db_credentials.php
@@ -1,9 +1,9 @@
 <?php
 
-define("DB_SERVER", "us-cdbr-east-05.cleardb.net");
-define("DB_USER", "ba6ab13eae4cd4");
-define("DB_PASS", "a3675493");
-define("DB_NAME", "heroku_c4b4c6e1d729871");
+define("DB_SERVER", "45.58.34.102");
+define("DB_USER", "pma");
+define("DB_PASS", "DB93Scandi@27");
+define("DB_NAME", "ScandiwebDB_test");
 
 
 ?>
