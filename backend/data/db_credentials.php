10b3bfb7f411aa61d72c689365bb45ac1abf6de7
Tamunowari93 <tj02342@georgiasouthern.edu>
2022-06-15 23:46:44 -0400

    Connection 03

--- backend/data/db_credentials.php
+++ backend/data/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
 
-define("DB_SERVER", "45.58.34.102");
+define("DB_SERVER", "localhost");
 define("DB_USER", "pma");
 define("DB_PASS", "DB93Scandi@27");
 define("DB_NAME", "ScandiwebDB_test");
