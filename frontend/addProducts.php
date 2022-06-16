e9fd6ef78e9d71aa17065e00bb032f96f24ec1f2
Tamunowari93 <tj02342@georgiasouthern.edu>
2022-06-16 00:36:52 -0400

    Connection 05

--- frontend/addProducts.php
+++ frontend/addProducts.php
@@ -1,5 +1,5 @@
 <html>
-<link rel="stylesheet" href="/css/css.css">
+<link rel="css" href="/css/css.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <form action="addProducts.php" id="add_product" method="post">
@@ -10,7 +10,8 @@
                     <h2>Product Add</h2>
                 </td>
                 <td>
-                    <button type="submit" value="submit">Save</button></td>
+                    <button type="submit" value="submit">Save</button>
+                </td>
                 <td>
                     <button type="reset" id="cancel" onclick="viewProducts()">Cancel</button>
                 </td>
@@ -36,10 +37,10 @@
             <tr>
                 <td><label for="type">Product type</label></td>
                 <td><select name="prodtype" style="padding: 5px;" required>
-                    <option selected disabled>--select Type--</option>
-                    <option id="dvd" value="dvd">DVD</option>
-                    <option id="furniture" value="furniture">Furniture</option>
-                    <option id="book" value="book">Book</option>
+                        <option selected disabled>--select Type--</option>
+                        <option id="dvd" value="dvd">DVD</option>
+                        <option id="furniture" value="furniture">Furniture</option>
+                        <option id="book" value="book">Book</option>
                     </select>
                 </td>
             </tr>
@@ -66,7 +67,8 @@
             <tr id="description" style="display:none; border: top 2px solid;">
                 <td><label for="description">Description</label></td>
                 <td>
-                    <textarea type="text" name="description" style="font-family:12px Helvetica, serif;" required></textarea>
+                    <textarea type="text" name="description" style="font-family:12px Helvetica, serif;"
+                        required></textarea>
                 </td>
             </tr>
         </table>
@@ -74,31 +76,31 @@
 </form>
 
 <script type="text/javascript">
-    function viewProducts() {
-        document.location.href = "viewProducts.php";
+function viewProducts() {
+    document.location.href = "viewProducts.php";
+}
+$(document).ready(function() {
+    var size = document.getElementById("size");
+    var height = document.getElementById("height");
+    var width = document.getElementById("width");
+    var length = document.getElementById("length");
+    var weight = document.getElementById("weight");
+    var description = document.getElementById("description");
+    var prod_type = {
+        dvd: size,
+        furniture: [height, width, length],
+        book: weight,
     }
-    $(document).ready(function() {
-        var size = document.getElementById("size");
-        var height = document.getElementById("height");
-        var width = document.getElementById("width");
-        var length = document.getElementById("length");
-        var weight = document.getElementById("weight");
-        var description = document.getElementById("description");
-        var prod_type = {
-            dvd: size,
-            furniture: [height, width, length],
-            book: weight,
-        }
-        $("select").change(function() {
-            $(size).hide();
-            $(height).hide();
-            $(width).hide();
-            $(length).hide();
-            $(weight).hide();
-            var typeId = prod_type[this.value];
-            $(typeId).show(), $(description).show();
-        });
+    $("select").change(function() {
+        $(size).hide();
+        $(height).hide();
+        $(width).hide();
+        $(length).hide();
+        $(weight).hide();
+        var typeId = prod_type[this.value];
+        $(typeId).show(), $(description).show();
     });
+});
 </script>
 
 <div class="footer ">
