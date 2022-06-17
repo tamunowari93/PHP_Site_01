cee454a010ee8b59a292a5fdaad86f165a7da1a2
Tamunowari93 <tj02342@georgiasouthern.edu>
2022-06-17 15:10:36 -0400

    Pages setup

--- scandiweb/frontend/index.php
+++ scandiweb/frontend/index.php
@@ -1,38 +1,4 @@
 <?PHP
-require_once'../backend/require.php';
+require_once'../backend/views/Pages.php';
 echo 'Scandiweb Start!';
 ?>
-<!DOCTYPE html>
-<html>
-
-<head>
-
-    <script>
-    function bgChange(bg) {
-        document.body.style.background = bg;
-    }
-    </script>
-</head>
-
-<body>
-
-    <h2>Change background color</h2>
-    <p>Mouse over the squares!</p>
-
-    <table style="width:300px;height:100px">
-        <tr>
-            <td onmouseover="bgChange(this.style.backgroundColor)" onmouseout="bgChange('transparent')"
-                style="background-color:Khaki">
-            </td>
-            <td onmouseover="bgChange(this.style.backgroundColor)" onmouseout="bgChange('transparent')"
-                style="background-color:PaleGreen">
-            </td>
-            <td onmouseover="bgChange(this.style.backgroundColor)" onmouseout="bgChange('transparent')"
-                style="background-color:Silver">
-            </td>
-        </tr>
-    </table>
-
-</body>
-
-</html>
