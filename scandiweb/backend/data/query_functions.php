36b7d52089ff055a0c3e7e1188b2c22f3848d1f9
Tamunowari93 <tj02342@georgiasouthern.edu>
2022-06-17 00:08:25 -0400

    test html with css

--- scandiweb/backend/data/query_functions.php
+++ scandiweb/backend/data/query_functions.php
@@ -1,19 +1,42 @@
 <?php
 
-  // Products  
-  // function add_product($sku, $name, $price, $prodtype, $description) {
+  function add_product($sku, $name, $price, $prodtype, $description) {
+    global $db;
+    $sql = "INSERT INTO products ";
+    $sql .= "(sku, name, price, prodtype, description) ";
+    $sql .= "VALUES (";
+    $sql .= "'" . db_escape($db, $sku) . "',";
+    $sql .= "'" . db_escape($db, $name) . "',";
+    $sql .= "'" . db_escape($db, $price) . "',";
+    $sql .= "'" . db_escape($db, $prodtype) . "',";
+    $sql .= "'" . db_escape($db, $description) . "'";
+    $sql .= ")";
+    $result = mysqli_query($db, $sql);
+  
+    // For INSERT statements, $result is true/false
+    if($result) {
+      return true;
+    } else {
+      // INSERT failed
+      echo mysqli_error($db);
+      db_disconnect($db);
+      exit;
+    }
+  } 
+
+  // function addDVD($sku, $name, $price, $size, $description) {
   //   global $db;
-  //   $sql = "INSERT INTO products ";
-  //   $sql .= "(sku, name, price, prodtype, description) ";
+  //   $sql = "INSERT INTO dvd ";
+  //   $sql .= "(sku, name, price, size, description) ";
   //   $sql .= "VALUES (";
   //   $sql .= "'" . db_escape($db, $sku) . "',";
   //   $sql .= "'" . db_escape($db, $name) . "',";
   //   $sql .= "'" . db_escape($db, $price) . "',";
-  //   $sql .= "'" . db_escape($db, $prodtype) . "',";
+  //   $sql .= "'" . db_escape($db, $size) . "',";
   //   $sql .= "'" . db_escape($db, $description) . "'";
   //   $sql .= ")";
   //   $result = mysqli_query($db, $sql);
-  
+    
   //   // For INSERT statements, $result is true/false
   //   if($result) {
   //     return true;
@@ -23,196 +46,172 @@
   //     db_disconnect($db);
   //     exit;
   //   }
-  // } 
-
-  function addDVD($sku, $name, $price, $size, $description) {
-    global $db;
-    $sql = "INSERT INTO dvd ";
-    $sql .= "(sku, name, price, size, description) ";
-    $sql .= "VALUES (";
-    $sql .= "'" . db_escape($db, $sku) . "',";
-    $sql .= "'" . db_escape($db, $name) . "',";
-    $sql .= "'" . db_escape($db, $price) . "',";
-    $sql .= "'" . db_escape($db, $size) . "',";
-    $sql .= "'" . db_escape($db, $description) . "'";
-    $sql .= ")";
-    $result = mysqli_query($db, $sql);
-    
-    // For INSERT statements, $result is true/false
-    if($result) {
-      return true;
-    } else {
-      // INSERT failed
-      echo mysqli_error($db);
-      db_disconnect($db);
-      exit;
-    }
-  }
+  // }
   
-  function addFurniture($sku, $name, $price, $dimensions, $description) {
-    global $db;
-    $sql = "INSERT INTO furniture ";
-    $sql .= "(sku, name, price, dimensions, description ) ";
-    $sql .= "VALUES (";
-    $sql .= "'" . db_escape($db, $sku) . "',";
-    $sql .= "'" . db_escape($db, $name) . "',";
-    $sql .= "'" . db_escape($db, $price) . "',";
-    $sql .= "'" . db_escape($db, $dimensions) . "',";
-    $sql .= "'" . db_escape($db, $description) . "'";
-    $sql .= ")";
-    $result = mysqli_query($db, $sql);
+  // function addFurniture($sku, $name, $price, $dimensions, $description) {
+  //   global $db;
+  //   $sql = "INSERT INTO furniture ";
+  //   $sql .= "(sku, name, price, dimensions, description ) ";
+  //   $sql .= "VALUES (";
+  //   $sql .= "'" . db_escape($db, $sku) . "',";
+  //   $sql .= "'" . db_escape($db, $name) . "',";
+  //   $sql .= "'" . db_escape($db, $price) . "',";
+  //   $sql .= "'" . db_escape($db, $dimensions) . "',";
+  //   $sql .= "'" . db_escape($db, $description) . "'";
+  //   $sql .= ")";
+  //   $result = mysqli_query($db, $sql);
     
-    // For INSERT statements, $result is true/false
-    if($result) {
-      return true;
-    } else {
-      // INSERT failed
-      echo mysqli_error($db);
-      db_disconnect($db);
-      exit;
-    }
-  }
+  //   // For INSERT statements, $result is true/false
+  //   if($result) {
+  //     return true;
+  //   } else {
+  //     // INSERT failed
+  //     echo mysqli_error($db);
+  //     db_disconnect($db);
+  //     exit;
+  //   }
+  // }
   
-  function addBook($sku, $name, $price,  $weight_kg, $description) {
-    global $db;
-    $sql = "INSERT INTO book ";
-    $sql .= "(sku, name, price, weight, description) ";
-    $sql .= "VALUES (";
-    $sql .= "'" . db_escape($db, $sku) . "',";
-    $sql .= "'" . db_escape($db, $name) . "',";
-    $sql .= "'" . db_escape($db, $price) . "',";
-    $sql .= "'" . db_escape($db, $weight_kg) . "',";
-    $sql .= "'" . db_escape($db, $description) . "'";
-    $sql .= ")";
-    $result = mysqli_query($db, $sql);
+  // function addBook($sku, $name, $price, $weight, $descriptions) {
+  //   global $db;
+  //   $sql = "INSERT INTO book ";
+  //   $sql .= "(sku, name, price, weight, description) ";
+  //   $sql .= "VALUES (";
+  //   $sql .= "'" . db_escape($db, $sku) . "',";
+  //   $sql .= "'" . db_escape($db, $name) . "',";
+  //   $sql .= "'" . db_escape($db, $price) . "',";
+  //   $sql .= "'" . db_escape($db, $weight) . "',";
+  //   $sql .= "'" . db_escape($db, $description) . "'";
+  //   $sql .= ")";
+  //   $result = mysqli_query($db, $sql);
     
-    // For INSERT statements, $result is true/false
-    if($result) {
-      return true;
-    } else {
-      // INSERT failed
-      echo mysqli_error($db);
-      db_disconnect($db);
-      exit;
-    }
-  }
+  //   // For INSERT statements, $result is true/false
+  //   if($result) {
+  //     return true;
+  //   } else {
+  //     // INSERT failed
+  //     echo mysqli_error($db);
+  //     db_disconnect($db);
+  //     exit;
+  //   }
+  // }
 
-  // Find products
-  function find_DVD() {
-    global $db;
-    $sql = "SELECT * FROM dvd ";
-    $result = mysqli_query($db, $sql);
+  // // Find products
+  // function getDVD() {
+  //   global $db;
+  //   $sql = "SELECT * FROM dvd ";
+  //   $result = mysqli_query($db, $sql);
     
-    if ($result==true) {
-      // Fetch one and one row
-      return $result;
-    }
-    else {
-      // Retrieve failed
-      echo mysqli_error($db);
-      mysqli_free_result($result);
-      db_disconnect($db);
-      exit;
-    }
-  }
-  function find_Furniture() {
-    global $db;
-    $sql = "SELECT * FROM furniture ";
-    $result = mysqli_query($db, $sql);
+  //   if ($result==true) {
+  //     // Fetch one and one row
+  //     return $result;
+  //   }
+  //   else {
+  //     // Retrieve failed
+  //     echo mysqli_error($db);
+  //     mysqli_free_result($result);
+  //     db_disconnect($db);
+  //     exit;
+  //   }
+  // }
+  // function getFurniture() {
+  //   global $db;
+  //   $sql = "SELECT * FROM furniture ";
+  //   $result = mysqli_query($db, $sql);
     
-    if ($result==true) {
-      // Fetch one and one row
-      return $result;
-    }
-    else {
-      // Retrieve failed
-      echo mysqli_error($db);
-      mysqli_free_result($result);
-      db_disconnect($db);
-      exit;
-    }
-  }
-  function find_Book() {
-    global $db;
-    $sql = "SELECT * FROM book ";
-    $result = mysqli_query($db, $sql);
+  //   if ($result==true) {
+  //     // Fetch one and one row
+  //     return $result;
+  //   }
+  //   else {
+  //     // Retrieve failed
+  //     echo mysqli_error($db);
+  //     mysqli_free_result($result);
+  //     db_disconnect($db);
+  //     exit;
+  //   }
+  // }
+  // function getBook() {
+  //   global $db;
+  //   $sql = "SELECT * FROM book ";
+  //   $result = mysqli_query($db, $sql);
     
-    if ($result==true) {
-      // Fetch one and one row
-      return $result;
-    }
-    else {
-      // Retrieve failed
-      echo mysqli_error($db);
-      mysqli_free_result($result);
-      db_disconnect($db);
-      exit;
-    }
-  }
+  //   if ($result==true) {
+  //     // Fetch one and one row
+  //     return $result;
+  //   }
+  //   else {
+  //     // Retrieve failed
+  //     echo mysqli_error($db);
+  //     mysqli_free_result($result);
+  //     db_disconnect($db);
+  //     exit;
+  //   }
+  // }
   
-  function delete_product($sku) {
-    global $db;
-    $sql = "DELETE FROM products ";
-    $sql .= "WHERE id='" . db_escape($db, $sku) . "' ";
-    $sql .= "LIMIT 1";
-    $result = mysqli_query($db, $sql);
+  // function deleteProduct($sku) {
+  //   global $db;
+  //   $sql = "DELETE FROM products ";
+  //   $sql .= "WHERE id='" . db_escape($db, $sku) . "' ";
+  //   $sql .= "LIMIT 1";
+  //   $result = mysqli_query($db, $sql);
 
-    // For DELETE statements, $result is true/false
-    if($result) {
-      return true;
-    } else {
-      // DELETE failed
-      echo mysqli_error($db);
-      db_disconnect($db);
-      exit;
-    }
-  }
+  //   // For DELETE statements, $result is true/false
+  //   if($result) {
+  //     return true;
+  //   } else {
+  //     // DELETE failed
+  //     echo mysqli_error($db);
+  //     db_disconnect($db);
+  //     exit;
+  //   }
+  // }
   
-  function mass_delete_DVD() {
-    global $db;
-    $sql = "DELETE FROM dvd ";
-    $result = mysqli_query($db, $sql);
+  // function massDelete() {
+  //   global $db;
+  //   $sql = "DELETE FROM dvd ";
+  //   $result = mysqli_query($db, $sql);
 
-    // For DELETE statements, $result is true/false
-    if($result) {
-      return true;
-    } else {
-      // DELETE failed
-      echo mysqli_error($db);
-      db_disconnect($db);
-      exit;
-    }
-  }
-  function mass_delete_FRN() {
-    global $db;
-    $sql = "DELETE FROM furniture ";
-    $result = mysqli_query($db, $sql);
+  //   // For DELETE statements, $result is true/false
+  //   if($result) {
+  //     return true;
+  //   } else {
+  //     // DELETE failed
+  //     echo mysqli_error($db);
+  //     db_disconnect($db);
+  //     exit;
+  //   }
+  // }
+  // function mass_delete_FRN() {
+  //   global $db;
+  //   $sql = "DELETE FROM furniture ";
+  //   $result = mysqli_query($db, $sql);
 
-    // For DELETE statements, $result is true/false
-    if($result) {
-      return true;
-    } else {
-      // DELETE failed
-      echo mysqli_error($db);
-      db_disconnect($db);
-      exit;
-    }
-  }
-  function mass_delete_BK() {
-    global $db;
-    $sql = "DELETE FROM book ";
-    $result = mysqli_query($db, $sql);
+  //   // For DELETE statements, $result is true/false
+  //   if($result) {
+  //     return true;
+  //   } else {
+  //     // DELETE failed
+  //     echo mysqli_error($db);
+  //     db_disconnect($db);
+  //     exit;
+  //   }
+  // }
+  // function mass_delete_BK() {
+  //   global $db;
+  //   $sql = "DELETE FROM book ";
+  //   $result = mysqli_query($db, $sql);
 
-    // For DELETE statements, $result is true/false
-    if($result) {
-      return true;
-    } else {
-      // DELETE failed
-      echo mysqli_error($db);
-      db_disconnect($db);
-      exit;
-    }
-  }
+  //   // For DELETE statements, $result is true/false
+  //   if($result) {
+  //     return true;
+  //   } else {
+  //     // DELETE failed
+  //     echo mysqli_error($db);
+  //     db_disconnect($db);
+  //     exit;
+  //   }
+  // }
   
 
 ?>
